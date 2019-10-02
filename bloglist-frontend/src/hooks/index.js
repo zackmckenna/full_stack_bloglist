8c16163b2b1e6d80b03d10c8cb42a4aae44b8bd5
Zachary Mckenna <zacharymckenna@Zacharys-MacBook-Pro.local>
2019-10-01 22:19:39 -0400

    Started working on useResource hook

--- bloglist-frontend/src/hooks/index.js
+++ bloglist-frontend/src/hooks/index.js
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import axios from 'axios'
+const baseUrl = '/api/blogs'
 
 export const useField = (type) => {
   const [value, setValue] = useState('')
@@ -18,3 +20,83 @@ export const useField = (type) => {
     reset
   }
 }
+
+/*export const useResource = (request) => {
+
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const createNew = async (newObject) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+}
+
+const deleteBlog = async id => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const response = await axios.delete(`${baseUrl}/${id}`, config)
+  return response.data
+}
+
+const likeBlog = async id => {
+  try{
+    const request = await axios.get(`${baseUrl}/${id.likedBlog}`)
+    const config = {
+      headers: { Authorization: token },
+    }
+    const updatedBlog = request.data
+    updatedBlog.likes +=1
+    const response = await axios.put(`${baseUrl}/${id.likedBlog}`, updatedBlog, config)
+    return response.data
+  } catch (exception){
+    console.log(exception)
+  }
+}
+
+const getByUserId = async userId => {
+  const request = await axios.get(baseUrl)
+  console.log(request)
+  const filteredBlogs = request.data.filter(blog => {
+    return blog.user !== undefined
+  })
+
+  console.log(filteredBlogs)
+  const userBlogs = filteredBlogs.filter(blog => {
+    if (blog.user.id === userId) {
+      return blog
+    }
+    return ''
+  })
+  console.log(userBlogs)
+  return userBlogs
+}
+
+const getById = async idd => {
+  const request = await axios.get(`${baseUrl}/${id}`)
+  console.log(request.data)
+  return request.data
+}
+
+return {
+  getAll,
+  getByUserId,
+  createNew,
+  setToken,
+  likeBlog,
+  deleteBlog,
+  getById
+}*/
+
+
+
